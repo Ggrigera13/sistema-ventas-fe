c09f14c21e8f68f9f2a1cea836364e7ec62bc867
Ggrigera13 <gian.caova@gmail.com>
2022-09-22 00:45:29 -0300

    [Initial] - Add custom style in Material

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,22 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatButtonModule } from '@angular/material/button';
-
-
-
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
-  ], exports: [
-    MatProgressBarModule,
-    MatIconModule,
-    MatDividerModule,
-    MatButtonModule
-  ]
+  ], exports: []
 })
 export class MaterialModule { }
